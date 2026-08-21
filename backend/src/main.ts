ab5ae7f45886aceefd7a1356a8e5757df36ff56e
Olamide Ganiyu <olamidefiyin18@gmail.com>
2026-08-21 12:18:38 +0000

    commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://graph-dev-frontend-qkbr27q0k-holarmideys-projects.vercel.app/', 'http://localhost:5173'],
+    origin: [
+      'https://graph-dev-frontend-qkbr27q0k-holarmideys-projects.vercel.app',
+      'http://localhost:5173',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
