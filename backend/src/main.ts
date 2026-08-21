a4f4fb81f35dbdacd6dbb55a44c88eff1c5bf18b
Olamide Ganiyu <olamidefiyin18@gmail.com>
2026-08-21 12:49:18 +0000

    commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'https://graph-dev-frontend-qkbr27q0k-holarmideys-projects.vercel.app',
+      'https://graph-dev-frontend-riw8o4zjv-holarmideys-projects.vercel.app',
+      'https://graph-dev-backend-na6qhsob1-holarmideys-projects.vercel.app',
       'http://localhost:5173',
     ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
